d2ac7240927e3b7f9002a4987e96db46889d5db6
bieel1503 <bieelziin1503@hotmail.com>
2020-04-12 13:01:50 -0300

    deleting files...

---
+++ Network/Skyblock/src/main/java/br/krabby/network/skyblock/Core.java
@@ -0,0 +1,12 @@
+package br.krabby.network.skyblock;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Core extends JavaPlugin {
+    public void onEnable(){
+    }
+
+    public static Core getInstance(){
+        return getPlugin(Core.class);
+    }
+}
